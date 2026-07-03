72c51428ca1959c0f5e46ebed0c46da8d9a7ccf3
lasith <it26100392@my.sliit.lk>
2026-07-03 14:14:45 +0000

    2 Player movements with basic functionality

--- Mainbody.c
+++ Mainbody.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-//#include <unistd.h>
+#include <ctype.h>
 
 #define Map_S 15
+#define Mxplayers 2
 
 typedef struct {
     char name[10];
@@ -21,6 +22,7 @@ typedef struct {
 //global 2d arrays
 char map[Map_S][Map_S];
 int Traps[Map_S][Map_S];
+
 player players[2];
 
 //func protyping 
@@ -35,6 +37,10 @@ void placeKeys();
 void placeDoors();
 void placeTraps();
 void placeplayers();
+int isValidMove(int row,int col);
+void processTile(int index);
+void movePlayer(int index);
+void gameloop();
 
 int main(){
     srand((time(NULL)));
@@ -53,10 +59,7 @@ do{
             system("clear");
             initializeMap();
             placeplayers();
-            printmap();
-            printf("\npressenter to continue ...");
-            while(getchar() != '\n');
-            getchar();
+            gameloop();
             break;
         case 2:
             //loadgame();
@@ -261,7 +264,8 @@ void placeTraps(){
 }
 
 void placeplayers(){
-    for(int i=0;i<2;i++){
+
+    for(int i=0;i<Mxplayers;i++){
         printf("Enter the %d player name :",i+1);
         scanf("%s",players[i].name);
        
@@ -284,4 +288,171 @@ void placeplayers(){
 
         }
     }
+}
+
+
+int isValidMove(int row,int col){
+    if(row<0 || row >= Map_S || col<0 || col>= Map_S){
+        return 0;
+    }
+
+    if (map[row][col] == "#"){
+        return 0;
+    }
+    return 1;
+}
+
+void processTile(int index){
+
+    int row = players[index].row;
+    int col = players[index].col;
+    
+    if(Traps[row][col] == 1){
+
+            players[index].health -= 20;
+            Traps[row][col] = 0;
+            printf("%s caught on a trap -20HP\n",players[index].name);
+
+            if(players[index].health <= 0){
+                players[index].health = 0;
+                players[index].isalive = 0;
+                printf("%s has been eliminated\n",players[index].name);
+            }
+    }
+
+    if(map[row][col] == 'T'){
+        players[index].score += 10;
+        map[row][col] = ' ';
+        printf("Nice JOB! %s found a treasure! +10 Score\n",players[index].name);
+
+    }
+
+    if(map[row][col] == 'H'){
+        players[index].health += 20;
+         if(players[index].health > 100){
+            players[index].health = 100;
+         }
+
+         map[row][col]=' ';
+         printf("%s got a Medikit! +20HP\n",players[index].name);
+
+    }
+
+    if(map[row][col]=='K'){
+        players[index].keys += 1;
+        map[row][col] = ' ';
+        printf("%s picked up a key !",players[index].name);
+    }
+
+}
+
+void movePlayer(int index){
+    char moves[10];
+
+    printf("\n %s enter moves (WASD)|max 4 moves :",players[index].name);
+    scanf("%s",moves);
+
+    if(strlen(moves)>4){
+        printf("Too many moves! Your turn hass been cancelld !!!");
+        return;
+    }
+      
+        int newrow = players[index].row;
+        int newcol = players[index].col;
+
+    for(int i = 0;i < strlen(moves);i++){
+        char move = toupper(moves[i]);
+
+        int newrow = players[index].row;
+        int newcol = players[index].col;
+
+
+        if(move == 'W'){
+            newrow--;
+        }else if(move == 'S'){
+            newrow++;
+        }else if(move =='A'){
+            newcol--;
+        }else if(move =='D'){
+            newcol++;
+        }else{
+            printf("You have entered a invalid move '%c' !!! SKIPPED !!!",moves[i]);
+            continue;
+        }
+    
+
+    if(map[newrow][newcol] == 'D'){
+        if(players[index].keys > 0){
+            players[index].keys--;
+            map[newrow][newcol] = ' ';
+            printf("%s unlocked a door!\n",players[index].name);
+        }else{
+            printf("%s need a key to open this door!",players[index].name);
+             continue;
+        }
+    }
+
+    if(isValidMove(newrow,newcol)){
+
+        map[players[index].row][players[index].col] = ' ';
+
+        players[index].row = newrow;
+        players[index].col = newcol;
+
+        processTile(index);
+
+        map[players[index].row][players[index].col] = players[index].symbol;
+
+    }else{
+      printf("Invalid Move blocked !!!\n");
+
+    }
+  }
+}
+
+void gameloop(){
+   int gamerunning = 1;
+
+   while(gamerunning){
+
+       for(int i=0;i<Mxplayers;i++){
+        if(!players[i].isalive) continue;
+        
+        system("clear");
+        printmap();
+
+        movePlayer(i);
+
+        int chestLeft = 0;
+        for(int r=0;r<15;r++){
+            for(int c=0;c<15;c++){
+                if(map[r][c] == 'T'){
+                    chestLeft++;
+                }
+                
+            }
+        }
+
+        if(chestLeft == 0){
+            printf("\n ALL TREASURES HAVE BEEN COLLECTED\n");
+            printf("---------GAME OVER-------\n");
+            gamerunning = 0;
+            break;
+        }
+
+        int aliveplayers = 0;
+        for(int j=0;j<Mxplayers;j++){
+            if(players[j].isalive){
+                aliveplayers++;
+            }}
+
+            if(aliveplayers == 0){
+                printf("\n All players has been eleminated !\n");
+                printf("-------GAME OVER-------\n");
+                gamerunning = 0;
+                break;
+            }
+       }
+   }
+
 }
